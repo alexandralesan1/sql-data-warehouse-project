1a65a23fa35209bea438a0f63c020b11bc2987aa
alexandralesan1 <163538340+alexandralesan1@users.noreply.github.com>
2026-04-16 20:33:44 +0300

    Refactor ddl_bronze.sql to simplify database setup
    Removed database creation and schema checks for DataWarehouse.

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,39 +1,4 @@
-USE master;
-GO
 
-IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
-BEGIN
-    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-    DROP DATABASE DataWarehouse;
-END;
-GO
-
-IF DB_ID('DataWarehouse') IS NULL
-BEGIN
-    CREATE DATABASE DataWarehouse;
-END;
-GO
-
-USE DataWarehouse;
-GO
-
-IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'bronze')
-BEGIN
-    EXEC('CREATE SCHEMA bronze');
-END;
-GO
-
-IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'silver')
-BEGIN
-    EXEC('CREATE SCHEMA silver');
-END;
-GO
-
-IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'gold')
-BEGIN
-    EXEC('CREATE SCHEMA gold');
-END;
-GO
 
 IF OBJECT_ID('bronze.crm_cust_info','U') IS NOT NULL DROP TABLE bronze.crm_cust_info;
 
